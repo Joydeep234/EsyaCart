c44c6edf90cb92ec0665cacb6af0e931d387e55a
joydeep234 <kundujoydeep597@gmail.com>
2024-11-18 09:57:43 +0530

    Final merge

--- EsyaCart/Pages/User/ShowProducts.cshtml.cs
+++ EsyaCart/Pages/User/ShowProducts.cshtml.cs
@@ -27,11 +27,12 @@ namespace EsyaCart.Pages.User
         public List<Products> productsEntity {get; set;} = new List<Products>();
         public string logoutchecksession {get;set;} = "";
         public double maxprice {get;set;} = 0;
-        public string cat_id {get;set;}
+        public int cat_id {get;set;}
         public Catagory catagory  {get;set;}
+        public  string qstring {get;set;}
         public Cart cart { get; set; } = new Cart();
         public int customerid { get; set; }
-        public async Task OnGet(string productcatagoryID,double pricerange)
+        public async Task OnGet(int productcatagoryID,double pricerange,string queryString)
         {
             try
             {
@@ -39,21 +40,88 @@ namespace EsyaCart.Pages.User
                 Response.Headers["Pragma"] = "no-cache";
                 Response.Headers["Expires"] = "0";
                 logoutchecksession = HttpContext.Session.GetString("UserSessionId");
-                cat_id = productcatagoryID;
-                var categoryId = int.Parse(productcatagoryID);
-                if(pricerange>0){
-                    productsEntity = await _context.Products
-                                                .Where(p => p.Category_Id == categoryId && p.Price <= pricerange)
-                                                .ToListAsync();
+
+                
+                if(!string.IsNullOrEmpty(queryString) && pricerange<1){ //string ,no range
+                    double price = 0;
+                    bool isPriceQuery = double.TryParse(queryString, out price);
+
+                    productsEntity = await (from p in _context.Products
+                                    join c in _context.Catagory on p.Category_Id equals c.Catagory_Id
+                                    where c.CatagoryName.Contains(queryString) ||
+                                        c.CategoryDescription.Contains(queryString) ||
+                                        p.ProductName.Contains(queryString) ||
+                                        p.Description.Contains(queryString) ||
+                                        (isPriceQuery && p.Price <= price)
+                                    select p).ToListAsync();
+                    if(productsEntity.Count<0)throw new Exception("products not found");
+                    catagory = await _context.Catagory.FirstOrDefaultAsync(c=>c.CatagoryName==queryString);
+                    if(catagory != null)cat_id = catagory.Catagory_Id;
+                    qstring = queryString;
+                        maxprice = await _context.Products.MaxAsync(p=>p.Price);
+                    return;
                 }
-                 else{
+                else if(pricerange>0 && queryString!=null){
+
+                    productsEntity = await (from p in _context.Products
+                            join c in _context.Catagory on p.Category_Id equals c.Catagory_Id
+                            where (c.CatagoryName.Contains(queryString) ||
+                                   c.CategoryDescription.Contains(queryString) ||
+                                   p.ProductName.Contains(queryString) ||
+                                   p.Description.Contains(queryString)) &&
+                                  p.Price <= pricerange
+                            select p).ToListAsync();
+                    if(productsEntity.Count<0)throw new Exception("products not found");
+                    catagory = await _context.Catagory.FirstOrDefaultAsync(c=>c.CatagoryName==queryString);
+                    if(catagory != null)cat_id = catagory.Catagory_Id;
+                    qstring = queryString;
+                    maxprice = await _context.Products.MaxAsync(p => p.Price);
+                    return;
+                }else if(productcatagoryID==0 && pricerange<1 && queryString==null) /*no string,no price,no cat*/
+                { 
+                    productsEntity = await _context.Products.ToListAsync();
+                    if(productsEntity.Count<0)throw new Exception("products not found");
+                    maxprice = await _context.Products.MaxAsync(p=>p.Price);
+                    return;
+                }else if(productcatagoryID==0 && pricerange>0 && queryString==null){ //no string and price
                     productsEntity = await _context.Products
-                                                .Where(p => p.Category_Id == categoryId)
+                                                .Where(p=>p.Price <= pricerange)
                                                 .ToListAsync();
-                 }   
-                    catagory = await _context.Catagory.FirstOrDefaultAsync(c=>c.Catagory_Id==categoryId);
-                    maxprice = await _context.Products.Where(p => p.Category_Id == categoryId).MaxAsync(p=>p.Price);
-                if(productsEntity.Count<0)throw new Exception("products not found");
+                    if(productsEntity.Count<0)throw new Exception("products not found");
+                    maxprice = await _context.Products.MaxAsync(p=>p.Price);
+                    return;
+                }
+
+                else if(productcatagoryID>0 && pricerange<1 && queryString==null){ //cat only
+                    productsEntity = await _context.Products
+                                                .Where(p => p.Category_Id == productcatagoryID)
+                                                .ToListAsync();
+                    if(productsEntity.Count<0)throw new Exception("products not found");
+                    catagory = await _context.Catagory.FirstOrDefaultAsync(c=>c.Catagory_Id==productcatagoryID);
+                    if(catagory != null)cat_id = catagory.Catagory_Id;
+                    
+                    maxprice = await _context.Products.Where(p => p.Category_Id == productcatagoryID).MaxAsync(p=>p.Price);
+                    return;
+                }
+                else if(productcatagoryID>0 && pricerange>0 && queryString==null){ //cat and range
+                    productsEntity = await _context.Products
+                                                .Where(p => p.Category_Id == productcatagoryID && p.Price <= pricerange)
+                                                .ToListAsync();
+                    if(productsEntity.Count<0)throw new Exception("products not found");
+                    catagory = await _context.Catagory.FirstOrDefaultAsync(c=>c.Catagory_Id==productcatagoryID);
+                    if(catagory != null)cat_id = catagory.Catagory_Id;
+                    
+                    maxprice = await _context.Products.Where(p => p.Category_Id == productcatagoryID).MaxAsync(p=>p.Price);
+                    return;
+                }
+                //  else {
+                //     productsEntity = await _context.Products
+                //                                 .Where(p => p.Category_Id == productcatagoryID)
+                //                                 .ToListAsync();
+                //                                 if(productsEntity.Count<0)throw new Exception("products not found");
+                //     return;
+                //  }   
+              
             }
             catch (Exception e)
             {
@@ -62,11 +130,11 @@ namespace EsyaCart.Pages.User
             
         }
 
-        public async Task<IActionResult> OnPostOnFilterRange(int catid){
+        public async Task<IActionResult> OnPostOnFilterRange(int catid,string qstring2){
             try
             {
                 if(slidermodel.SelectedPrice>0){
-                    return RedirectToPage("../User/ShowProducts",new {productcatagoryID = catid,pricerange = slidermodel.SelectedPrice});
+                    return RedirectToPage("../User/ShowProducts",new {productcatagoryID = catid,pricerange = slidermodel.SelectedPrice,queryString = qstring2});
                 }
                 return Page();
             }
